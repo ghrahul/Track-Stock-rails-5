0e24e7b0e33afbb828ac48ab23734612cc739c8c
Rahul Ghosh <ghrahul2016@gmail.com>
2019-07-15 14:09:45 +0530

    bug fixed for stock price updation and stockquote api functionality updated for its new version

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -2,14 +2,21 @@ class UserStocksController < ApplicationController
   
   def create
     stock = Stock.find_by_ticker(params[:stock_ticker])
+    StockQuote::Stock.new(api_key: 'pk_e8793f14b4b3494c97abf63680b8ba21')
+    var = StockQuote::Stock.quote(params[:stock_ticker])
+
+    stock_new = Stock.new(ticker: var.symbol, name: var.company_name, last_price: var.close)
     if stock.blank?
-      StockQuote::Stock.new(api_key: 'pk_e8793f14b4b3494c97abf63680b8ba21')
-      var = StockQuote::Stock.quote(params[:stock_ticker])
-      stock = Stock.new(ticker: var.symbol, name: var.company_name, last_price: var.close)
-      stock.save
+      stock_new.save
     end
-    @user_stock = UserStock.create(user: current_user, stock: stock)
-    flash[:success] = "Stock #{@user_stock.stock.name} was successfully added"
+
+    if UserStock.where(user: current_user, stock: stock_new).exists?
+      UserStock.update(user: current_user, stock: stock_new)
+      flash[:success] = "Stock #{stock_new.name} was successfully updated"
+    else
+      @user_stock = UserStock.create(user: current_user, stock: stock_new)
+      flash[:success] = "Stock #{@user_stock.stock.name} was successfully added"
+    end
     redirect_to my_portfolio_path
   end
   
